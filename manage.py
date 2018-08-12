4d361a9bd6f68ed527c4af48d81a45f3b1e3edd7
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-08-12 00:45:34 -0300

    refact manage.py

--- manage.py
+++ manage.py
@@ -8,13 +8,10 @@ def cli():
     pass
 
 
-@cli.command('get_manga_list')
+@cli.command()
 def get_manga_list():
     worker = WorkerMangaReader()
     worker.get_manga_list()
-
-
-cli.add_command(get_manga_list)
 
 
 if __name__ == '__main__':
